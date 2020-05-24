f48a9433465fe932fa57422647937c897c710897
Alvin Chang <alvinchang@mapacode.tw>
2020-05-24 11:23:57 +0800

    使用指令稿的方式更新

--- generate_patch.bash
+++ generate_patch.bash
@@ -2,33 +2,27 @@
 
 patch=./release/patch.db
 
-rm -rf ${patch}
-sqlite3 -line ${patch} < db/patch_schemas.sql
+git diff -U0 --output=release/patch_0.0.2 0.0.1 0.0.4 db/origin_songs.csv 
 
-git diff -U0 --output=release/patch_0.0.2 0.0.1 0.0.2 db/origin_songs.csv 
+grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
 
-
-#grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
-
-grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO songs (sn, deleted) VALUES ( %d, 1);\n", $1}' > patch_0.0.2.cmd
+grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM songs WHERE sn = %d;\n", $1}' > patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/patch_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO songs \
-(sn, name, fuzzy_name, word_count, kjbcode, language, category, artist_sn, artist_name, artist_fuzzy_name, loudness, vocal_channel, deleted) VALUES \
-( %d, \"%s\", \"%s\", %d, \"%s\", %d, %d, %d, \"%s\", \"%s\", %d, %d, %d);\n", $1, $2, $3, $4, $6, $7, $8, $9, $10, $11, $12, $13, $14}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO songs \
+(sn, name, fuzzy_name, word_count, path, kjbcode, language, category, artist_sn, artist_name, artist_fuzzy_name, loudness, vocal_channel) VALUES \
+( %d, \"%s\", \"%s\", %d, \" %s\", \"%s\", %d, %d, %d, \"%s\", \"%s\", %d, %d);\n", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13}' >>  patch_0.0.2.cmd
 
 #################
 
 git diff -U0 --output=release/artists_0.0.2 0.0.1 0.0.2 db/origin_artists.csv 
 
-#grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
-
-grep -e ^-[1-9] release/artists_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO artists (sn, deleted) VALUES ( %d, 1);\n", $1}' >> patch_0.0.2.cmd
+grep -e ^-[1-9] release/artists_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM artists WHERE sn = %d;\n", $1}' >> patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/artists_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO artists \
-(sn, name, fuzzy_name, word_count, category, country, deleted) VALUES \
-( %d, \"%s\", \"%s\", %d, %d, %d, %d);\n", $1, $2, $3, $4, $5, $6, $8}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO artists \
+(sn, name, fuzzy_name, word_count, category, country, path) VALUES \
+( %d, \"%s\", \"%s\", %d, %d, %d, \" %s\");\n", $1, $2, $3, $4, $5, $6, $7}' >>  patch_0.0.2.cmd
 
 #################
 
@@ -36,12 +30,12 @@ git diff -U0 --output=release/song_categories_0.0.2 0.0.1 0.0.2 db/origin_song_c
 
 #grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
 
-grep -e ^-[1-9] release/song_categories_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO song_categories (sn, deleted) VALUES ( %d, 1);\n", $1}' >> patch_0.0.2.cmd
+grep -e ^-[1-9] release/song_categories_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM song_categories WHERE sn = %d;\n", $1}' >> patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/song_categories_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO song_categories \
-(sn, name, priority, deleted) VALUES \
-( %d, \"%s\", %d, 0);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO song_categories \
+(sn, name, priority) VALUES \
+( %d, \"%s\", %d);\n", $1, $2}' >>  patch_0.0.2.cmd
 
 ###############
 
@@ -49,12 +43,12 @@ git diff -U0 --output=release/song_languages_0.0.2 0.0.1 0.0.2 db/origin_song_la
 
 #grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
 
-grep -e ^-[1-9] release/song_languages_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO song_languages (sn, deleted) VALUES ( %d, 1);\n", $1}' >> patch_0.0.2.cmd
+grep -e ^-[1-9] release/song_languages_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM song_languages WHERE sn = %d;\n", $1}' >> patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/song_languages_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO song_languages \
-(sn, name, priority, deleted) VALUES \
-( %d, \"%s\", %d, 0);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO song_languages \
+(sn, name, priority) VALUES \
+( %d, \"%s\", %d);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
 
 #################
 
@@ -62,12 +56,12 @@ git diff -U0 --output=release/artist_categories_0.0.2 0.0.1 0.0.2 db/origin_arti
 
 #grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
 
-grep -e ^-[1-9] release/artist_categories_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO artist_categories (sn, deleted) VALUES ( %d, 1);\n", $1}' >> patch_0.0.2.cmd
+grep -e ^-[1-9] release/artist_categories_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM artist_categories WHERE sn = %d;\n", $1}' >> patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/artist_categories_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO artist_categories \
-(sn, name, priority, deleted) VALUES \
-( %d, \"%s\", %d, 0);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO artist_categories \
+(sn, name, priority) VALUES \
+( %d, \"%s\", %d);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
 
 #################
 
@@ -75,18 +69,17 @@ git diff -U0 --output=release/artist_countries_0.0.2 0.0.1 0.0.2 db/origin_artis
 
 #grep -e ^-[1-9] release/patch_0.0.2 | sed 's/^-//g'
 
-grep -e ^-[1-9] release/artist_countries_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "INSERT INTO artist_countries (sn, deleted) VALUES ( %d, 1);\n", $1}' >> patch_0.0.2.cmd
+grep -e ^-[1-9] release/artist_countries_0.0.2 | sed 's/^-//g' | awk -F"|" '{printf "DELETE FROM artist_countries WHERE sn = %d;\n", $1}' >> patch_0.0.2.cmd
 
 grep -e ^+[1-9] release/artist_countries_0.0.2 \
-| sed 's/^+//g' | awk -F"|" '{printf "REPLACE INTO artist_countries \
-(sn, name, priority, deleted) VALUES \
-( %d, \"%s\", %d, 0);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
+| sed 's/^+//g' | awk -F"|" '{printf "INSERT INTO artist_countries \
+(sn, name, priority) VALUES \
+( %d, \"%s\", %d);\n", $1, $2, $3}' >>  patch_0.0.2.cmd
 
 ##############
 
-echo "INSERT INTO versions (name, major, minor, patch) VALUES ('song', 0, 0, 2);">> patch_0.0.2.cmd
+echo "UPDATE versions SET major = 0, minor = 0, patch = 2 WHERE name = 'song';">> patch_0.0.2.cmd
 echo "VACUUM;" >> patch_0.0.2.cmd
 
-sqlite3 -line ${patch} < patch_0.0.2.cmd
 
 
