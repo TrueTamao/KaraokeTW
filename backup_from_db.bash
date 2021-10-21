235b9c69d2e22c959fa9073f9240f3fad1cb4965
Alvin Chang <alvinchang@mapacode.tw>
2021-10-21 15:57:48 +0800

    更新中文歌 loudness，vocal channel，duration

--- backup_from_db.bash
+++ backup_from_db.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo -u postgres psql -d origin_songs -c "DROP TABLE IF EXISTS youtube_videos;"
+sudo -u postgres psql -d origin_songs -c "DROP TABLE IF EXISTS pglogical_test_ids;"
+sudo -u postgres psql -d origin_songs -c "DROP TABLE IF EXISTS pglogical_test_sns;"
+
 sudo su postgres -c "vacuumdb -f origin_songs"
 sudo su postgres -c "pg_dump -d origin_songs -O -f ~/song.dump"
 cp -f /var/lib/postgresql/song.dump db
