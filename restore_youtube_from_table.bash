3c23d196499b443735983c8c96eaf6e7ddc2db55
Alvin Chang <alvinchang@mapacode.tw>
2021-11-17 16:02:45 +0800

    Modify YouTube Scripts

--- restore_youtube_from_table.bash
+++ restore_youtube_from_table.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+read -p "Source File ? " sourceFile
+
+if [ ! -f "$sourceFile" ] ; then
+    echo -e "\e[1;5;31m Invalid Source File \e[0m"
+    exit 1
+fi
+
 read -p "Restore YouTube Table (Will Drop YouTube Table) (Y/N) ? [N]" restoreTable
 
 if [ "$restoreTable" = "Y" ] || [ "$restoreTable" = "y" ] ; then
@@ -11,7 +18,7 @@ if [ "$restoreTable" = "Y" ] || [ "$restoreTable" = "y" ] ; then
     if [ "$restoreMakeSure" = "Y" ] || [ "$restoreMakeSure" = "y" ] ; then
 
         psql -d song -c "DELETE FROM youtube_videos;"
-        psql song < db/youtube.dump
+        psql song < $sourceFile
         psql -d song -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO mapa;"
         psql -d song -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA PUBLIC TO mapa;"
         echo -e "\e[1;5;32m Done !! \e[0m"
