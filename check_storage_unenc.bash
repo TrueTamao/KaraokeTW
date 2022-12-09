0ec259ef8bc69322bb440a4b8a207f14ea08180e
TrueTamao <tamaoserizawa123@gmail.com>
2022-12-09 08:11:47 +0000

    modify song 09/12/22

--- check_storage_unenc.bash
+++ check_storage_unenc.bash
@@ -1,27 +1,35 @@
 #!/bin/bash
 shopt -s -o nounset
 i=0
-kjbstart=10001
-kjbend=591732
+kjbstart=$1
+kjbend=$2
+logfile=no_uenc_$kjbstart-$kjbend.txt
 
-echo "Start check for unenc not found from " $kjbstart " to " $kjbend > no_unenc.txt
+echo "Start check for unenc not found from " $kjbstart " to " $kjbend > $logfile
 
 for ((i=$kjbstart; i<= $kjbend; i=i+1))
 do
   #echo "current code: " + $i
+  #printf "gsutil -m cp591732 -r gs://new_songs/new_song_videos_44100_25fps/%s gs://song_videos_standard/indonesia_video_out_44100_25fps/ \n" $kjbcode >> cp_to_storage.bash
+
   kjbcode=$i
-  printf "gsutil -m cp591732 -r gs://new_songs/new_song_videos_44100_25fps/%s gs://song_videos_standard/indonesia_video_out_44100_25fps/ \n" $kjbcode >> cp_to_storage.bash
+  m3u8_file=gs://song_videos_standard/indonesia_video_out_44100_25fps/$kjbcode/$kjbcode.m3u8
+  gsutil -q stat $m3u8_file
+  m3u8_status=$?
+  
+  if (($m3u8_status == 0)); then
 
-  for video_path in videoHlsLd
-  do
-    file_path=gs://song_videos_standard/indonesia_video_out_44100_25fps/$kjbcode/$video_path/video_unenc.m3u8
-    echo "filepath: " + $file_path
-    gsutil -q stat $file_path
-    status=$?
+    for video_path in videoHlsLd
+    do
+      file_path=gs://song_videos_standard/indonesia_video_out_44100_25fps/$kjbcode/$video_path/video_unenc.m3u8
+      #echo "filepath: " + $file_path
+      gsutil -q stat $file_path
+      status=$?
 
-    if (($status != 0)); then
-      echo $kjbcode $video_path "  Unenc File does not exist status " $status >> no_unenc.txt
-    fi
-  done
+      if (($status != 0)); then
+        echo $kjbcode $video_path "  Unenc File does not exist status " $status >> $logfile
+      fi
+    done
+  fi
 done
 
